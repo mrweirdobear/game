27443d135093b443cccff9cd771592a41c10992c
Cesar Franco <atlas230231@yahoo.com>
2020-07-12 15:17:45 -0700

    moving the character

--- site.js
+++ site.js
@@ -9,10 +9,12 @@ let me explain what is happening here.
 //we need to get our hero container
 var heroContainer = document.getElementById('hero-container');
 //we need get our hero div
-var heroes = document.getElementsByClassName('hero');
+// var heroes = document.getElementsByClassName('hero');
+var hero = document.getElementById('hero');
 //create variable to hold frame number/name
 var currentFrame = "stand";
 var counter = 0;
+var leftPosition = 0;
 
 function updateFrame(){
     if(currentFrame == "stand"){ //change frame from stand to step
@@ -32,7 +34,36 @@ function updateFrame(){
         //update currentFrame to new frame name
         currentFrame = "stand";
     }
+
+    if(counter == 6){
+        moveCharacter();
+        //do something
+        counter = 0;
+    }
+    else{
+        counter++;
+    }
 }
+
+function moveCharacter(){
+    //check if moving right
+    if(movingRight)
+    {
+        leftPosition = leftPosition + 25;
+        hero.style.left = leftPosition + "px";
+    }
+    else if (movingLeft){
+        //check if moving left
+        leftPosition = leftPosition - 25;
+        hero.style.left = leftPosition + "px";
+    } else if (movingUp)
+    {
+        //check if moving up 
+        leftPosition = leftPosition - 25;
+        hero.style.top = leftPosition + "px";
+    }
+}
+
 
 function updateMultipleFrames(){
     if(currentFrame == "stand"){ //change frame from stand to step
@@ -76,7 +107,7 @@ function updateMultipleFrames(){
 }
 
 /* for a game */
-setInterval(updateMultipleFrames, 170);
+setInterval(updateFrame, 170);
 
 function growBody(){
     var newHero = document.createElement("div");
